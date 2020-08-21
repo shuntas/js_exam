78c3c4e5fc93e18ad1865cda6d0c478406b30e2d
shuntas <shunta.suzuki@di-v.co.jp>
2020-08-21 10:43:21 +0900

    セミコロン付与

--- app/javascript/count.js
+++ app/javascript/count.js
@@ -2,8 +2,8 @@ function count() {
   const articleText  = document.getElementById("article_text");
   articleText.addEventListener("keyup", () => {
     const charNum  = document.getElementById("char_num");
-    countVal = articleText.value.length
-    charNum.innerHTML = `${countVal}文字`
-  })
+    countVal = articleText.value.length;
+    charNum.innerHTML = `${countVal}文字`;
+  });
 }
 window.addEventListener("load", count);
--- app/javascript/hover.js
+++ app/javascript/hover.js
@@ -1,14 +1,13 @@
 function hover() {
-  const articles = document.querySelectorAll(".article")
+  const articles = document.querySelectorAll(".article");
   articles.forEach((article) => {
     article.addEventListener('mouseover', () => {
       article.setAttribute("style", "background-color:#F1940B;");
-    })
+    });
     article.addEventListener('mouseout', () => {
       article.removeAttribute("style", "background-color:#FFFFFF;");
-    })
-  })
+    });
+  });
 }
-window.addEventListener("load", hover);
-// 非同期で投稿した分にも適用したい場合は以下を使用する
-// setInterval(hover, 1000);
+
+setInterval(hover, 1000);
